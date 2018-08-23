e60caf3eb9c2dfa1a3e1b16453443a100c7e222f
Kevin W <kjoewill@gmail.com>
2018-08-23 04:40:35 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,6 +58,11 @@ def all_winter_holiday_supplies(holiday_hash)
 
 end
 
+
+def format_symbol(symbol)
+  s = symbol.to_s
+end
+
 def all_supplies_in_holidays(holiday_hash)
   # iterate through holiday_hash and print items such that your readout resembles:
   # Winter:
