4a2de8f6e7dd13aeb64a8ad9a8de190f7eeeb7a3
Kevin W <kjoewill@gmail.com>
2018-08-23 04:27:28 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -81,8 +81,22 @@ def all_holidays_with_bbq(holiday_hash)
 end
 
 
+  holidays = {
+     :winter => {
+       :christmas => ["Lights", "Wreath"],
+       :new_years => ["Party Hats"]
+     },
+     :summer => {
+       :fourth_of_july => ["Fireworks", "BBQ"]
+     },
+     :fall => {
+       :thanksgiving => ["Turkey"]
+     },
+     :spring => {
+       :memorial_day => ["BBQ"]
+     }
+   }
 
-
-
+all_supplies_in_holidays(holiday_hash)
 
 
